f299c3355f814d88fe2f25fc21e47f91bb250ba0
Gamygams1234 <gamyburgos@gmail.com>
2020-05-13 16:28:37 -0700

    Working with conditional statements

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <?php
-	// comparisons & booleans (true or false)
-	//echo true // echo's "1"
-	//echo false // echo's ""
 
-    // this will echo all of the variables
-	// numbers
-	// echo 5 < 10;
-	// echo 5 > 10;
-	//echo 5 == 10;
-	//echo 10 == 10;
-	//echo 5 != 10;
-	//echo 5 <= 5;
-	//echo 5 >= 5;
+	// conditional statements
+	
+	// $price = 20;
 
-// depending on the first letter of the  word
+	// if($price < 30){
+	// 	echo 'if condition met';
+	// } elseif($price === 20) {
+	// 	echo 'elseif condition met';
+	// } else {
+	// 	echo 'condition not met';
+    // }
+    
+    $products = [
+		['name' => 'shiny star', 'price' => 20],
+		['name' => 'green shell', 'price' => 10],
+		['name' => 'red shell', 'price' => 15],
+		['name' => 'gold coin', 'price' => 5],
+		['name' => 'lightning bolt', 'price' => 40],
+		['name' => 'banana skin', 'price' => 2]
+	];
 
-	// strings
-	// echo 'shaun' < 'yoshi';
-	echo 'shaun' > 'yoshi';
-	//echo 'shaun' > 'Shaun';
-	//echo 'mario' == 'mario';
-	//echo 'mario' == 'Mario';
+	foreach($products as $product){
 
-	// loose vs strict equal comparison
+		if($product['price'] < 15 && $product['price'] > 2){
+			echo $product['name'] . '<br />';
+		}
 
-	//echo 5 == '5';
-	//echo 5 === '5';
-	//echo 5 === 5;
+		// if($product['price'] > 20 || $product['price'] < 10){
+		// 	echo $product['name'] . '<br />';
+		// }
 
-	//echo true == 1;
-	// echo true === 1;
-
+	}
 
 ?>
 <!DOCTYPE html>
