20a05dbfe5d7260b268f64fc6f96e8692ac70e8f
Gamygams1234 <gamyburgos@gmail.com>
2020-05-14 15:05:56 -0700

    Getting the data from the database

--- index.php
+++ index.php
@@ -2,13 +2,29 @@
 <?php 
 // connect to database
 
-$conn = mysqli_connect('localhost', 'gamy', 'test1234', 'gamygams_pizza');
+$conn = mysqli_connect('localhost', 'gamy', 'test1234', 'gamygams_pizza'); // taking 4 items. (localhost, username, passwoed, the name of the actual database)
 
-// check connection
-// this will warn us if there is an error at all in our database
+
 if (!$conn){
 	echo "Connection error: " . mysqli_connect_error();
 }
+// write query for all of the pizzas
+$sql = 'SELECT title, ingredients, id FROM pizzas';// we can use * for all of the items, however, we are only getting certain items
+
+
+// make query and get results
+$result = mysqli_query($conn, $sql); // using the connection and sql as variables
+
+// fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC); // this will return an associative array
+
+// free the $result from memory (good practise)
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($conn);
+
+print_r($pizzas);
 
 ?>
 
